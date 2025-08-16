7eabe9dcbee196ca116de8e70c19a6b0626b4a43
mirjalolbekkosimov <mirjalolbekkosimov@gmail.commirjalolbekqosimov778mirjalolbekqosimov778>
2025-08-16 18:56:34 +0500

    update storybook build

--- assets/react-18-BnV9c1KO.js
+++ assets/react-18-BnV9c1KO.js
@@ -1,4 +1,4 @@
-import{a as kv,r as Fc}from"./iframe-BpmXwrpd.js";var wc={exports:{}},ie={},Wc={exports:{}},$c={};/**
+import{a as kv,r as Fc}from"./iframe-BPteB_e-.js";var wc={exports:{}},ie={},Wc={exports:{}},$c={};/**
  * @license React
  * scheduler.production.js
  *
